96db8fcf4502920ea5b162d3a555651e72d8f25f
KMS <epicblue@hanmail.net>
2021-08-21 16:14:25 +0900

    typescript config finished

---
+++ typescript/dist/index.js
@@ -0,0 +1,13 @@
+//Basic Types
+let id = 5;
+let company = "Traversy Media";
+let isPublished = true;
+let x = "hello";
+let ids = [1, 2, 3, 4];
+let mixed = [1, 2, 3, 'yaho'];
+//Tuple
+let person = [2, 'muyaho', false];
+console.log(person);
+//Tuple Array
+let persons = [person, [3, 'pussy', true]];
+//# sourceMappingURL=index.js.map
---
+++ typescript/src/index.ts
@@ -0,0 +1,17 @@
+//Basic Types
+let id: number = 5;
+let company:string = "Traversy Media"; 
+let isPublished:boolean=true;
+let x :any = "hello";
+
+
+let ids : number[] = [1,2,3,4];
+let mixed : (string|number)[] = [1,2,3,'yaho'];
+
+//Tuple
+let person :[number, string, boolean]= [2,'muyaho',false];
+console.log(person);
+//Tuple Array
+let persons: [number,string,boolean][] = [person,[3,'pussy',true]];
+
+
