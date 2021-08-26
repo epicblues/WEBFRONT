48b77787df07e69a0d3a09785c13e2741d25bfe9
KimMinsung <epicblue@hanmail.net>
2021-08-26 13:20:22 +0900

    for in - all object for of - iterable object

--- js/05-flowcontrol.js
+++ js/05-flowcontrol.js
@@ -29,17 +29,17 @@ function printStars(numOfStar) {
 console.log('-----------------------------------------');
 
 
-function printStartsRae(numOfStar) {
+function printStarRaeVer(numOfStar) {
     let str = '';
     for (let i = 0; i < numOfStar; i++) {
         str += '*';
     }
 
     for (let j = numOfStar; j>0; j--) {
-        console.log(str.substr(0, j));
+        console.log(str.substring(0, j));
     }
 }
-printStartsRae(10);
+printStarRaeVer(5);
 
 let p = 5;
 while (p > 0) {
@@ -54,4 +54,31 @@ while (p > 0) {
 }
 
 
+// ES6 : for ... in
+// 객체의 Property를 순회한다.
+let obj = {
+    name: "홍길동",
+    age: 28,
+    job: '도적',
+    gender:'남성'
+}
 
+let array = ['kms', 2, 4, 'wtf', obj,true,null,undefined];
+
+
+for (let key in obj) {
+    console.log(key, obj[key]);
+}
+
+
+for (let key in array) {
+    console.log(key, array[key]);
+}
+
+// for of 는 반복 가능 객체의 요소를 하나씩 끄집어낸다.
+// for of 는 순회 객체 에서만 사용할 수 있다.
+// Error : obj는 순환 객체가 아니다.
+
+for (let value of array) {
+    console.log(value);
+}
